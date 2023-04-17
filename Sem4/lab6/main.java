9016bcb5c159c6728bad0f2a2d43bbcf81c82893
Chazelam <andreilaymzin@gmail.com>
2023-04-17 17:18:09 +0300

    All work fine

--- Sem4/lab6/main.java
+++ Sem4/lab6/main.java
@@ -4,9 +4,29 @@ import java.awt.*;
 import java.awt.event.*;
 import java.lang.Math;
 
-class Plot extends Frame {
+class Plot extends Frame implements ActionListener{
+    TextField setA;
+    TextField setB;
+    Button MyDraw;
+    int A = 0; //5
+    int B = 0; //11
+
     Plot(){
-        setSize(450, 450);
+        setSize(700, 450);
+        setLayout(null);
+        setA = new TextField(10);
+        setA.setBounds(10, 38, 60, 25);
+        add(setA);
+
+        setB = new TextField(10);
+        setB.setBounds(10, 63, 60, 25);
+        add(setB);
+
+        MyDraw = new Button("Старт");
+        MyDraw.setBounds(10,138,60,25);
+        MyDraw.addActionListener(this);
+        add(MyDraw);
+
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent ev) {
                 System.exit(0);
@@ -16,7 +36,7 @@ class Plot extends Frame {
     }
 
     public void paint(Graphics g){
-        int x, y, A = 5, B = 11;
+        int x, y;
         double h = 15;
         for (int i = 1; i < 500; i++){
             double t = i*3.1415/250;
@@ -25,11 +45,16 @@ class Plot extends Frame {
             g.drawOval(x, y, 1, 1);
         }
     }
-}
 
+    public void actionPerformed(ActionEvent e){
+        if (e.getActionCommand() == "Старт"){    
+            A = Integer.parseInt(setA.getText());
+            B = Integer.parseInt(setB.getText());
+            repaint();
+        }
+    }
 
-class Program{
-    public static void main(String[] args) {
+    public static void main(String args []) {
         new Plot();
     }
 }
